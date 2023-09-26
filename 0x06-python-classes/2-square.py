45319bf0a0c856aba899bbfdd56b48057e131805
MentalSep <chahidjava@hotmail.com>
2023-09-26 13:58:08 +0100

    add task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module creates a class Square that defines a square"""
+
+
+class Square:
+    """Square class with a private attribute - size"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """Initializes the size variable as a private instance"""
+        self.__size = size
