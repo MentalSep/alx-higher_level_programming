a54e8ba24c2f4616c4409eb304984f794c5d2d85
MentalSep <chahidjava@hotmail.com>
2023-10-02 18:26:50 +0100

    fic pycodestyle file 6 task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,8 +4,8 @@
 
 class Rectangle:
     """class Rectangle definition."""
-    
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializes a new Rectangle."""
         self.width = width
