e51a8b7d640f4b97ec041aa9ee68bd3ae998f1e4
MentalSep <chahidjava@hotmail.com>
2023-10-09 13:51:42 +0100

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that contains BaseGeometry class"""
+
+
+class BaseGeometry:
+    """An empty basic geometry class"""
+    def area(self):
+        """Method that raises an exception"""
+        raise Exception("area() is not implemented")
