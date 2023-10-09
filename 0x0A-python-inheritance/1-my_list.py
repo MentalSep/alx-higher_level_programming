dd567e243bc74d3a58e983b110e38205c0174d8f
MentalSep <chahidjava@hotmail.com>
2023-10-09 13:41:10 +0100

    add task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Module that contains MyList class """
+"""Module that contains MyList class"""
 
 
 class MyList(list):
-    """ Class that inherits from list """
+    """Class that inherits from list"""
     def print_sorted(self):
-        """ Method that prints the list, but sorted (ascending sort) """
+        """Method that prints the list, but sorted (ascending sort)"""
         print(sorted(self))
