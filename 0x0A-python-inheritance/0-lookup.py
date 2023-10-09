2fa274af352c428cc9c126c58886caf0472f342d
MentalSep <chahidjava@hotmail.com>
2023-10-09 13:13:58 +0100

    add task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+""" Module that contains a function that returns a list of available
+attributes and methods of an object """
+
+
+def lookup(obj):
+    """ Function that returns a list of available attributes
+    and methods of an object """
+    return dir(obj)
