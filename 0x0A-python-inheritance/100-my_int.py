45a37645bb72fd664cd7706e09a3d8b72b33cbaf
MentalSep <chahidjava@hotmail.com>
2023-10-09 14:27:14 +0100

    add task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/sur/bin/python3
+"""Module that contains a class named MyInt"""
+
+
+class MyInt(int):
+    """Class that inherits from int"""
+    def __eq__(self, other):
+        """Method that returns the opposite of =="""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Method that returns the opposite of !="""
+        return super().__eq__(other)
