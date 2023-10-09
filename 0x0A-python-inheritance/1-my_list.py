a841218883682fcdfc8bf9eb520dde3e5aa51fe6
MentalSep <chahidjava@hotmail.com>
2023-10-09 13:26:39 +0100

    add task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,4 @@ class MyList(list):
 
     def print_sorted(self):
         """ Method that prints the list, but sorted (ascending sort) """
-        print(sorted(self))
+        print(sorted(list(self)))
