57f116caa4891c81c54be6fdb433d9d66290db72
MentalSep <chahidjava@hotmail.com>
2023-10-10 14:44:51 +0100

    test task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,6 +3,11 @@
 line by line and computes metrics"""
 import sys
 
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+lineCount = 0
+
 
 def print_stats(file_size, status_codes):
     """Prints the stats"""
@@ -12,23 +17,20 @@ def print_stats(file_size, status_codes):
             print("{}: {}".format(key, value))
 
 
-def main():
-    """Main function"""
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-    file_size = 0
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            tokens = line.split()
-            file_size += int(tokens[-1])
-            status_codes[tokens[-2]] += 1
-            if i == 10:
-                print_stats(file_size, status_codes)
-                i = 0
-    except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
-        raise
-
-
 if __name__ == "__main__":
-    main()
+    try:
+        for line in sys.stdin:
+            lineCount += 1
+            data = line.split()
+            status = data[-2]
+            file_size = data[-1]
+            if status in status_codes:
+                status_codes[status] += 1
+            total_size += int(file_size)
+            if lineCount == 10:
+                print_stats(total_size, status_codes)
+                lineCount = 0
+        print_stats(total_size, status_codes)
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
