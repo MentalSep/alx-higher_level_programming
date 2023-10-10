44b5f418d5fd039c7d7af9ec65bde0693f91e734
MentalSep <chahidjava@hotmail.com>
2023-10-10 14:25:49 +0100

    add task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,32 +4,33 @@ line by line and computes metrics"""
 import sys
 
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
 lineCount = 0
 
-try:
-    for line in sys.stdin:
-        lineCount += 1
-        data = line.split()
-        try:
-            status_codes[data[-2]] += 1
-        except [IndexError, ValueError, TypeError]:
-            pass
-        try:
-            total_size += int(data[-1])
-        except [ValueError, TypeError]:
-            pass
-        if lineCount == 10:
-            print("File size: {}".format(total_size))
-            for key, value in sorted(status_codes.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
-            lineCount = 0
-except KeyboardInterrupt:
-    print("File size: {}".format(total_size))
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
-    raise
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            lineCount += 1
+            data = line.split()
+            try:
+                status_codes[data[-2]] += 1
+            except [IndexError, ValueError, TypeError]:
+                pass
+            try:
+                total_size += int(data[-1])
+            except [ValueError, TypeError]:
+                pass
+            if lineCount == 10:
+                print("File size: {}".format(total_size))
+                for key, value in sorted(status_codes.items()):
+                    if value != 0:
+                        print("{}: {}".format(key, value))
+                lineCount = 0
+    except KeyboardInterrupt:
+        print("File size: {}".format(total_size))
+        for key, value in sorted(status_codes.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
+        raise
