a57e35eba2075c37526fca6710d673ec659e4c45
MentalSep <chahidjava@hotmail.com>
2023-10-10 15:08:56 +0100

    add task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,8 +4,14 @@ line by line and computes metrics"""
 import sys
 
 
-def print_stats(file_size, status_codes):
-    """Prints the stats"""
+def print_stats(status_codes, file_size):
+    """
+    prints stats
+
+    Arguments:
+        status_codes {dict} -- [dictionary of status codes]
+        file_size {int} -- [file size]
+    """
     print("File size: {}".format(file_size))
     for key, value in sorted(status_codes.items()):
         if value != 0:
@@ -16,10 +22,10 @@ if __name__ == "__main__":
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                     '403': 0, '404': 0, '405': 0, '500': 0}
     total_size = 0
-    lineCount = 0
+    LineCount = 0
     try:
         for line in sys.stdin:
-            lineCount += 1
+            LineCount += 1
             data = line.split()
             try:
                 total_size += int(data[-1])
@@ -29,9 +35,9 @@ if __name__ == "__main__":
                 status_codes[data[-2]] += 1
             except [KeyError, IndexError]:
                 pass
-            if lineCount % 10 == 0:
-                print_stats(total_size, status_codes)
-        print_stats(total_size, status_codes)
+            if LineCount % 10 == 0:
+                print_stats(status_codes, total_size)
+        print_stats(status_codes, total_size)
     except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
+        print_stats(status_codes, total_size)
         raise
