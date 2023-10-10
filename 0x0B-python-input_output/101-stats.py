91b07bd5b3d7d6dd7b14879e046be2e44b5413fb
MentalSep <chahidjava@hotmail.com>
2023-10-10 14:51:05 +0100

    add task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,11 +3,6 @@
 line by line and computes metrics"""
 import sys
 
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-lineCount = 0
-
 
 def print_stats(file_size, status_codes):
     """Prints the stats"""
@@ -18,6 +13,10 @@ def print_stats(file_size, status_codes):
 
 
 if __name__ == "__main__":
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
+    total_size = 0
+    lineCount = 0
     try:
         for line in sys.stdin:
             lineCount += 1
@@ -25,17 +24,16 @@ if __name__ == "__main__":
             status = data[-2]
             file_size = data[-1]
             try:
-                    status_codes[status] += 1
+                status_codes[status] += 1
             except [KeyError, IndexError]:
                 pass
             try:
                 total_size += int(file_size)
             except [ValueError, TypeError]:
                 pass
-            if lineCount == 10:
+            if lineCount % 10 == 0:
                 print_stats(total_size, status_codes)
-                lineCount = 0
         print_stats(total_size, status_codes)
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, EOFError):
         print_stats(total_size, status_codes)
         raise
