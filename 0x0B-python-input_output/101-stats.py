3299d2fb16f1746caa98fe3b1df971e51a6fcb9a
MentalSep <chahidjava@hotmail.com>
2023-10-10 15:10:16 +0100

    revert to old solution

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,14 +4,8 @@ line by line and computes metrics"""
 import sys
 
 
-def print_stats(status_codes, file_size):
-    """
-    prints stats
-
-    Arguments:
-        status_codes {dict} -- [dictionary of status codes]
-        file_size {int} -- [file size]
-    """
+def print_stats(file_size, status_codes):
+    """Prints the stats"""
     print("File size: {}".format(file_size))
     for key, value in sorted(status_codes.items()):
         if value != 0:
@@ -22,22 +16,19 @@ if __name__ == "__main__":
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                     '403': 0, '404': 0, '405': 0, '500': 0}
     total_size = 0
-    LineCount = 0
+    lineCount = 0
     try:
         for line in sys.stdin:
-            LineCount += 1
+            lineCount += 1
             data = line.split()
-            try:
-                total_size += int(data[-1])
-            except [ValueError, IndexError]:
-                pass
-            try:
-                status_codes[data[-2]] += 1
-            except [KeyError, IndexError]:
-                pass
-            if LineCount % 10 == 0:
-                print_stats(status_codes, total_size)
-        print_stats(status_codes, total_size)
+            status = data[-2]
+            file_size = data[-1]
+            if status in status_codes:
+                status_codes[status] += 1
+            total_size += int(file_size)
+            if lineCount % 10 == 0:
+                print_stats(total_size, status_codes)
+        print_stats(total_size, status_codes)
     except KeyboardInterrupt:
-        print_stats(status_codes, total_size)
+        print_stats(total_size, status_codes)
         raise
