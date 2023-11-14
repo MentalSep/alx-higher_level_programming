7f938c87d28e4401cb91281880facc24d3157b72
MentalSep <chahidjava@hotmail.com>
2023-11-14 17:40:32 +0100

    add more tasks

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- create a tabe second_table and inserts multiple records
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 7),
+(4, "George", 0);
