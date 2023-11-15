3bb5963f5691d2263238a473998428ca629075c0
MentalSep <chahidjava@hotmail.com>
2023-11-15 15:40:40 +0100

    add task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS count
 FROM tv_genres 
 RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY count DESC, genre ASC;
+ORDER BY count DESC;
 
