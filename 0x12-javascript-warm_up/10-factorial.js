447cb4f8946474f90dec51730119fed55746ace1
MentalSep <chahidjava@hotmail.com>
2023-12-11 22:32:29 +0100

    add task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
