2149d22d349407f10b984536e19664361596f89f
MentalSep <chahidjava@hotmail.com>
2023-12-17 21:03:27 +0100

    add task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""state_fetch_all module"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    session = Session(engine)
+    result = session.query(State).filter(State.name == (sys.argv[4],))
+    print(result.id if result else "Noy found")
+    Session.close()
