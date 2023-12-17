79bc0b4331cc17a53b110cb1f63f48e956eb0576
MentalSep <chahidjava@hotmail.com>
2023-12-17 20:44:36 +0100

    add task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""select state Module"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+                   INNER JOIN states ON states.id=cities.state_id \
+                   WHERE states.name = %s ORDER BY cities.id", (sys.argv[4],))
+    results = cursor.fetchall()
+    mylist = list(results[0] for line in results)
+    print(*mylist, sep=", ")
+    db.close()
