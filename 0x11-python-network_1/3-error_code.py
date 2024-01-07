cd0690207ac411917e8447818a0fb420800b4190
MentalSep <chahidjava@hotmail.com>
2024-01-07 19:54:26 +0100

    add task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""sends a request to the passed URL and displays the body of the response"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
