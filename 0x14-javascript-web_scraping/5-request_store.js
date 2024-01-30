8eb80751a9e749d24ab4051e875870bdbcd768ce
MentalSep <chahidjava@hotmail.com>
2024-01-30 18:58:59 +0100

    add task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(path, body, 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+    });
+  }
+});
