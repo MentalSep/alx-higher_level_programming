db98e8e88740c9ec54caf125189c516fec160656
MentalSep <chahidjava@hotmail.com>
2024-01-30 19:43:01 +0100

    add task 8

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,14 +8,12 @@ request(url, function (err, response, body) {
     console.log(err);
   } else {
     const characters = JSON.parse(body).characters;
-    characters.forEach(characterUrl => {
-      request(characterUrl, function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (!err) {
           console.log(JSON.parse(body).name);
         }
       });
-    });
+    }
   }
 });
